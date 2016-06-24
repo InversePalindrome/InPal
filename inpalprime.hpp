f27fb10200322dee8e0a6d9d655521f8c46c75f4
InversePalindrome <bryantrianarueda@gmail.com>
2016-06-24 13:52:51 -0500

    Update inpalprime.hpp

--- inpalprime.hpp
+++ inpalprime.hpp
@@ -18,16 +18,15 @@ public:
     unsigned long long pn_find(unsigned long long n);
     unsigned long long pn_count(unsigned long long l);
     long double pn_den(long double h);
-    unsigned long long n_maxfac(unsigned long long m);
     bool pn_test(unsigned long long k);
-    
+    unsigned long long n_maxfac(unsigned long long m);
+
     
 private:
     unsigned long long maxprime=0;
     unsigned long long primecount=0;
     long double primeden;
     unsigned long long maxfac=0;
-    
     
 };
 
