864f76fb6c800e6df8427ada8d62379dca20981b
InversePalindrome <bryantrianarueda@gmail.com>
2016-06-24 13:41:52 -0500

    Update inpalprime.hpp

--- inpalprime.hpp
+++ inpalprime.hpp
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Inverse Palindrome. All rights reserved.
 //
 
+
 #ifndef inpalprime_hpp
 #define inpalprime_hpp
 
@@ -15,19 +16,20 @@ class inpalprime
 
 public:
     unsigned long long pn_find(unsigned long long n);
-    unsigned long long n_maxfac(unsigned long long m);
     unsigned long long pn_count(unsigned long long l);
     long double pn_den(long double h);
+    unsigned long long n_maxfac(unsigned long long m);
     bool pn_test(unsigned long long k);
+    
     
 private:
     unsigned long long maxprime=0;
-    unsigned long long maxfac=0;
     unsigned long long primecount=0;
     long double primeden;
-   
+    unsigned long long maxfac=0;
+    
+    
 };
 
 
 #endif /* inpalprime_hpp */
-
