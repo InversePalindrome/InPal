7e3a29b9e345fb8b4c6a7ae660a9f4e48156177c
InversePalindrome <bryantrianarueda@gmail.com>
2016-07-03 10:38:46 -0500

    Update inpalprime.hpp

--- src/inpalprime.hpp
+++ src/inpalprime.hpp
@@ -16,22 +16,21 @@
 
 namespace inpal
 {
-    long long max_prime(long long n);
-    long long count_primes(long long n);
-    long double prime_density(long double h);
-    bool prime_test(long long p);
-    bool twin_test(long long p);
-    bool cousin_test(long long p);
-    bool sexy_test(long long p);
-    long long max_palprime(long long n);
-    long long max_factor(long long f);
-    long long count_factors(long long f);
+    std::size_t max_prime(std::size_t n);
+    std::size_t count_primes(std::size_t n);
+    double prime_density(double h);
+    bool prime_test(std::size_t p);
+    bool twin_test(std::size_t p);
+    bool cousin_test(std::size_t p);
+    bool sexy_test(std::size_t p);
+    std::size_t max_palprime(std::size_t n);
+    std::size_t max_factor(std::size_t f);
+    std::size_t count_factors(std::size_t f);
     
-    std::vector<bool> prime_sieve(long long m);
-    std::vector<long long> factorizer(long long f);
-    bool pal_test(long long n);
+    std::vector<bool> prime_sieve(std::size_t m);
+    std::vector<std::size_t> factorizer(std::size_t f);
+    bool pal_test(std::size_t n);
 }
-
 
 
 #endif /* inpalprime_hpp */
