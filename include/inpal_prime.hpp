4cda6feeb62ec0529a9c3771cd7e3923f2ccf4f5
InversePalindrome <bryantrianarueda@gmail.com>
2016-07-15 20:15:05 -0500

    added general purpose functions

--- include/inpal_prime.hpp
+++ include/inpal_prime.hpp
@@ -19,6 +19,10 @@ namespace inpal
     class prime
     {
     public:
+        //general purpose functions
+        static std::vector<std::size_t> factor_list(std::size_t num);
+        static std::vector<std::size_t> prime_list(std::size_t num);
+        //special purpose functions
         static std::size_t max_prime(std::size_t range);
         static std::size_t prime_count(std::size_t range);
         static double prime_density(double range);
@@ -28,10 +32,10 @@ namespace inpal
         static bool sexy_test(std::size_t num);
         static std::size_t max_palprime(std::size_t range);
         static std::size_t max_factor(std::size_t num);
-        static std::size_t count_factors(std::size_t num);
+        static std::size_t factor_count(std::size_t num);
     private:
+        //utility algorithms
         static std::vector<bool> prime_sieve(std::size_t range);
-        static std::vector<std::size_t> factorizer(std::size_t num);
         static bool pal_test(std::size_t num);
     };
 }
