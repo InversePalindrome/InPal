20e2107b03bf456a977fbec02248d47736463eff
InversePalindrome <bryantrianarueda@gmail.com>
2016-07-22 12:11:53 -0500

    switch from static members to namespaces

--- src/inpal_prime.cpp
+++ src/inpal_prime.cpp
@@ -1,13 +1,14 @@
 //
 //  inpal_prime.cpp
-//  InversePalindrome
+//  Inverse Palindrome Library
 //
-//  Created by Bryan Triana on 7/13/16.
+//  Created by Bryan Triana on 7/21/16.
 //  Copyright © 2016 Inverse Palindrome. All rights reserved.
 //
 
 
 #include "inpal_prime.hpp"
+#include "inpal_algorithm.hpp"
 #include <cmath>
 #include <vector>
 #include <string>
@@ -23,132 +24,6 @@ std::vector<std::size_t> inpal::prime::prime_list(std::size_t range)
     for(std::size_t i=2; i<=range; i++) if(primes[i]) p_list.push_back(i);
     
     return p_list;
-}
-
-
-std::vector<std::size_t> inpal::prime::factor_list(std::size_t num)
-{
-    std::vector<std::size_t> p_fac;
-    std::size_t prime_factor = 2;
-    
-    //trial division
-    while(prime_factor<=num)
-    {
-        while(num%prime_factor==0)
-        {
-            p_fac.push_back(prime_factor);
-            num=num/prime_factor;
-        }
-        prime_factor += prime_factor==2 ? 1 : 2;
-    }
-    
-    return p_fac;
-}
-
-
-std::size_t inpal::prime::prime_locate(std::size_t pos)
-{
-    //index starts at 1 instead of 0, eg 1st prime is 2
-    pos = pos-1;
-    
-    //return values for input less or equal to 10
-    const auto small_primes = prime_list(29);
-    if(pos<10) return small_primes[pos];
-    
-    //denotes the limit of the sieve
-    std::size_t limit = pos*log(pos)+pos*log(log(pos));
-    const auto primes = prime_list(limit);
-  
-    return primes[pos];
-}
-
-
-std::size_t inpal::prime::max_prime(std::size_t num)
-{
-    const auto primes = prime_sieve(num);
-    auto it = std::find(primes.rbegin(), primes.rend(), true);
-    
-    return primes.size()-std::distance(primes.rbegin(), it)-1;
-}
-
-
-std::size_t inpal::prime::prime_count(std::size_t range)
-{
-    const auto primes = prime_sieve(range);
-    
-    return std::count(primes.begin(), primes.end(), true);
-}
-
-
-double inpal::prime::prime_density(double range)
-{
-    return prime_count(range)/range;
-}
-
-
-bool inpal::prime::prime_test(std::size_t num)
-{
-    if(num!=2 && num%2==0) return false;
-    
-    const std::size_t iteration = 20;
-    std::size_t s = num-1;
-    while(s%2==0) s/=2;
-    
-    for(std::size_t i=0; i<iteration; i++)
-    {
-        std::size_t a = rand()%(num-1)+1;
-        std::size_t b = s;
-        std::size_t mod = modulo(a,b,num);
-        
-        while(b!=num-1 && mod!=1 && mod!=num-1)
-        {
-            mod=mulmod(mod,mod,num);
-            b*=2;
-        }
-        if(mod!=num-1 && b%2==0) return false;
-    }
-    
-    return true;
-}
-
-
-bool inpal::prime::twin_test(std::size_t num)
-{
-   return prime_test(num) && (prime_test(num-2) || prime_test(num+2));
-}
-
-
-bool inpal::prime::cousin_test(std::size_t num)
-{
-   return  prime_test(num) && (prime_test(num-4) || prime_test(num+4));
-}
-
-
-bool inpal::prime::sexy_test(std::size_t num)
-{
-   return num!=3 && prime_test(num) && (prime_test(num-6) || prime_test(num+6));
-}
-
-
-std::size_t inpal::prime::max_palprime(std::size_t num)
-{
-    const auto primes = prime_sieve(num);
-    
-    for(std::size_t i=num; i>=2; --i) if(primes[i] && pal_test(i)) return i;
-    
-    return 2;
-}
-
-
-std::size_t inpal::prime::max_factor(std::size_t num)
-{
-    return factor_list(num).back();
-}
-
-
-std::size_t inpal::prime::factor_count(std::size_t num)
-{
-    return factor_list(num).size();
 }
 
 
@@ -201,46 +76,127 @@ std::vector<bool> inpal::prime::prime_sieve(std::size_t range)
 }
 
 
-std::size_t inpal::prime::modulo(std::size_t a, std::size_t b, std::size_t c)
+std::vector<std::size_t> inpal::prime::factor_list(std::size_t num)
 {
-    std::size_t x = 1;
+    std::vector<std::size_t> p_fac;
+    std::size_t prime_factor = 2;
     
-    while(b>0)
+    //trial division
+    while(prime_factor<=num)
     {
-        if(b%2==1) x=(x*a%c);
-        a=(a*a)%c;
-        b/=2;
+        while(num%prime_factor==0)
+        {
+            p_fac.push_back(prime_factor);
+            num=num/prime_factor;
+        }
+        prime_factor += prime_factor==2 ? 1 : 2;
     }
     
-    return x%c;
+    return p_fac;
 }
 
 
-std::size_t inpal::prime::mulmod(std::size_t a, std::size_t b, std::size_t c)
+std::size_t inpal::prime::prime_locate(std::size_t pos)
 {
-    std::size_t x = 0;
-    std::size_t y = a%c;
+    //index starts at 1 instead of 0, eg 1st prime is 2
+    pos = pos-1;
     
-    while(b>0)
-    {
-        if(b%2==1) x=(x+y)%c;
-        y=(y*2)%c;
-        b/=2;
-    }
+    //return values for input less or equal to 10
+    const auto small_primes = prime_list(29);
+    if(pos<10) return small_primes[pos];
     
-    return x%c;
+    //denotes the limit of the sieve
+    std::size_t limit = pos*log(pos)+pos*log(log(pos));
+    const auto primes = prime_list(limit);
+    
+    return primes[pos];
 }
 
 
-bool inpal::prime::pal_test(std::size_t num)
+std::size_t inpal::prime::max_prime(std::size_t num)
 {
-    std::string rev = std::to_string(num);
+    const auto primes = prime_sieve(num);
+    auto it = std::find(primes.rbegin(), primes.rend(), true);
     
-    //checks if half the reverse of rev is equal to the other half of rev
-    if(std::equal(rev.begin(), rev.begin()+rev.size()/2, rev.rbegin()))
+    return primes.size()-std::distance(primes.rbegin(), it)-1;
+}
+
+
+std::size_t inpal::prime::prime_count(std::size_t range)
+{
+    const auto primes = prime_sieve(range);
+    
+    return std::count(primes.begin(), primes.end(), true);
+}
+
+
+double inpal::prime::prime_density(double range)
+{
+    return prime_count(range)/range;
+}
+
+
+bool inpal::prime::prime_test(std::size_t num)
+{
+    if(num!=2 && num%2==0) return false;
+    
+    const std::size_t iteration = 20;
+    std::size_t s = num-1;
+    while(s%2==0) s/=2;
+    
+    for(std::size_t i=0; i<iteration; i++)
     {
-        return true;
+        std::size_t a = rand()%(num-1)+1;
+        std::size_t b = s;
+        std::size_t mod = algorithm::modulo(a,b,num);
+        
+        while(b!=num-1 && mod!=1 && mod!=num-1)
+        {
+            mod=algorithm::mulmod(mod,mod,num);
+            b*=2;
+        }
+        if(mod!=num-1 && b%2==0) return false;
     }
     
-    return false;
+    return true;
+}
+
+
+bool inpal::prime::twin_test(std::size_t num)
+{
+    return prime_test(num) && (prime_test(num-2) || prime_test(num+2));
+}
+
+
+bool inpal::prime::cousin_test(std::size_t num)
+{
+    return  prime_test(num) && (prime_test(num-4) || prime_test(num+4));
+}
+
+
+bool inpal::prime::sexy_test(std::size_t num)
+{
+    return num!=3 && prime_test(num) && (prime_test(num-6) || prime_test(num+6));
+}
+
+
+std::size_t inpal::prime::max_palprime(std::size_t num)
+{
+    const auto primes = prime_sieve(num);
+    
+    for(std::size_t i=num; i>=2; --i) if(primes[i] && algorithm::pal_test(i)) return i;
+    
+    return 2;
+}
+
+
+std::size_t inpal::prime::max_factor(std::size_t num)
+{
+    return factor_list(num).back();
+}
+
+
+std::size_t inpal::prime::factor_count(std::size_t num)
+{
+    return factor_list(num).size();
 }
