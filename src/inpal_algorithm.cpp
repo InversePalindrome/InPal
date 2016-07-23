501f49a3ac48733572419060e7fd124592daddfe
InversePalindrome <bryantrianarueda@gmail.com>
2016-07-23 12:59:18 -0500

    implemented pollard rho for prime factorization

--- src/inpal_algorithm.cpp
+++ src/inpal_algorithm.cpp
@@ -10,6 +10,19 @@
 #include "inpal_algorithm.hpp"
 
 
+std::size_t inpal::algorithm::gcd(std::size_t a, std::size_t b)
+{
+  while(b!=0)
+  {
+      std::size_t r = a%b;
+      a=b;
+      b=r;
+  }
+    
+    return a;
+}
+
+
 std::size_t inpal::algorithm::modulo(std::size_t a, std::size_t b, std::size_t c)
 {
     std::size_t x = 1;
@@ -38,6 +51,75 @@ std::size_t inpal::algorithm::mulmod(std::size_t a, std::size_t b, std::size_t c
     }
     
     return x%c;
+}
+
+
+std::size_t inpal::algorithm::pollard_rho(std::size_t num)
+{
+    const std::size_t m = 1000;
+    std::size_t a, x, y, ys, r, q, d;
+    
+    do a = rand()%num;
+    while(a==0||a==num-2);
+    
+    y = rand()%num;
+    d = 1;
+    q = 1;
+    r = 1;
+    
+    do
+    {
+        x=y;
+        for(std::size_t i=0; i<=r; i++) y=mulmod(y,a,num);
+        std::size_t j = 0;
+        
+        do
+        {
+            ys = y;
+            for(std::size_t i=0; i<=std::min(m, r-j); i++)
+            {
+                y=mulmod(y,a,num);
+                q *= (std::max(x,y)-std::min(x,y))%num;
+            }
+            d=gcd(q,num);
+            j+=m;
+        }
+        while(j<r && d==1);
+        r*=2;
+    }
+    while(d==1);
+    
+    if(d==num)
+    {
+        do
+        {
+            ys=mulmod(ys,a,num);
+            d=gcd(std::max(x, ys)-std::min(x,ys), num);
+        }
+        while(d==1);
+    }
+    
+    return d;
+}
+
+
+std::vector<std::size_t> inpal::algorithm::trial_division(std::size_t num)
+{
+    std::vector<std::size_t> p_fac;
+    std::size_t prime_factor = 2;
+    
+    //trial division
+    while(prime_factor<=num)
+    {
+        if(num%prime_factor==0)
+        {
+            p_fac.push_back(prime_factor);
+            num=num/prime_factor;
+        }
+        prime_factor += prime_factor==2 ? 1 : 2;
+    }
+    
+    return p_fac;
 }
 
 
