a3bdaf3e08f618e5c73f3ddea8e74beb5603dbec
InversePalindrome <bryantrianarueda@gmail.com>
2016-07-23 12:54:27 -0500

    implemented pollard rho for prime factorization

--- include/inpal_algorithm.hpp
+++ include/inpal_algorithm.hpp
@@ -12,15 +12,20 @@
 
 #include <cstdlib>
 #include <string>
+#include <vector>
 
 
 namespace inpal
 {
    namespace algorithm
     {
+        std::size_t gcd(std::size_t a, std::size_t b);
         std::size_t modulo(std::size_t a, std::size_t b, std::size_t c);
         std::size_t mulmod(std::size_t a, std::size_t b, std::size_t c);
+        std::size_t pollard_rho(std::size_t num);
+        std::vector<std::size_t> trial_division(std::size_t num);
         bool pal_test(std::size_t num);
+
     }
 }
 
