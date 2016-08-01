821f091cb3a61a5b1c23d16e8e6f693a224df1f1
InversePalindrome <bryantrianarueda@gmail.com>
2016-08-01 12:17:27 -0500

    Update inpal_prime.cpp

--- src/inpal_prime.cpp
+++ src/inpal_prime.cpp
@@ -115,12 +115,12 @@ std::size_t inpal::prime::prime_locate(std::size_t pos)
     //index starts at 1 instead of 0, eg 1st prime is 2
     pos = pos-1;
     
-    //return values for input less or equal to 10
-    const auto small_primes = prime_list(29);
-    if(pos<10) return small_primes[pos];
+    //return values for input less or equal to 13
+    const auto small_primes = prime_list(43);
+    if(pos<14) return small_primes[pos];
     
     //denotes the limit of the sieve
-    std::size_t limit = pos*log(pos)+pos*log(log(pos));
+    const std::size_t limit = pos*log(pos)+pos*log(log(pos));
     const auto primes = prime_list(limit);
     
     return primes[pos];
